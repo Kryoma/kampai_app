037a049e4570ce4de86d8059339bf82377f6896e
Kryoma <ryoma.kuboshi@gmail.com>
2022-11-30 23:15:40 +0900

    再コミット

--- app/src/main/java/com/example/kampaiapp/MainActivity.kt
+++ app/src/main/java/com/example/kampaiapp/MainActivity.kt
@@ -69,7 +69,7 @@ fun App() {
         )
         Spacer(modifier = Modifier.height(56.dp))
         Image(
-            modifier = Modifier
+            modifier = Modifiergit
                 .size(250.dp, 160.dp)
                 .clip(RoundedCornerShape(10.dp))
                 .border(
