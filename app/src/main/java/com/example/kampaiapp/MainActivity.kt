dc1a1add531e27d24a788d2199cb73fac2381353
Kryoma <ryoma.kuboshi@gmail.com>
2022-11-30 23:13:38 +0900

    FirstCommit
    UIプロトタイプ作成

---
+++ app/src/main/java/com/example/kampaiapp/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.kampaiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.kampaiapp.ui.theme.KampaiAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KampaiAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    TopAppBar(
+        backgroundColor = Color.DarkGray,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "乾杯App",
+            color = Color.White,
+        )
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "日本",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(modifier = Modifier.height(56.dp))
+        Image(
+            modifier = Modifier
+                .size(250.dp, 160.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .border(
+                    BorderStroke(1.dp, Color.Black)
+                ),
+            painter = painterResource(R.drawable._00px_flag_of_japan_jpg),
+            contentDescription = "国旗"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "乾杯",
+            fontSize = 50.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = "kampai",
+            fontSize = 16.sp,
+            color = Color.LightGray
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(250.dp, 115.dp)
+                .clip(RoundedCornerShape(10.dp)),
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.textButtonColors(
+                backgroundColor = Color.DarkGray,
+                contentColor = Color.White
+            )
+        )
+        {
+            Text(
+                text = "飲む",
+                fontSize = 26.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KampaiAppTheme {
+        App()
+    }
+}
